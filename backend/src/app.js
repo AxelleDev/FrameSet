a53b6eed547425c6ebb2ac2a49adb7f4c6321c6a
Axelle <139721736+AxelleDev@users.noreply.github.com>
2026-03-12 14:57:49 +0100

    added Helmet middleware to set secure HTTP response headers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const authRoutes = require('./routes/auth.routes');
 const userRoutes = require('./routes/user.routes');
@@ -7,6 +8,7 @@ const projectsRoutes = require('./routes/projects.routes');
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
